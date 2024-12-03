19a0c36be16e2746d6c68752dabb887b3b17b742
Raymond D. Lee, M.Sc. <raydlee.msc@gmail.com>
2024-12-03 16:22:56 +0700

    stage 2

--- project/app/main.py
+++ project/app/main.py
@@ -1,8 +1,34 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from sqlmodel import select
+from sqlmodel import Session
+
+from app.db import get_session, init_db
+from app.models import Song, SongCreate
 
 app = FastAPI()
+
+
+@app.on_event("startup")
+def on_startup():
+    init_db()
 
 
 @app.get("/ping")
 async def pong():
     return {"ping": "pong!"}
+
+
+@app.get("/songs", response_model=list[Song])
+def get_songs(session: Session = Depends(get_session)):
+    result = session.execute(select(Song))
+    songs = result.scalars().all()
+    return [Song(name=song.name, artist=song.artist, id=song.id) for song in songs]
+
+
+@app.post("/songs")
+def add_song(song: SongCreate, session: Session = Depends(get_session)):
+    song = Song(name=song.name, artist=song.artist)
+    session.add(song)
+    session.commit()
+    session.refresh(song)
+    return song
